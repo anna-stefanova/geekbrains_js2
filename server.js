3846b82c65cdbddbbe77a587162294397396a7f8
Anna <astefanova267@gmail.com>
2020-11-12 19:00:14 +0300

    adding image

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const server = http.createServer((req, res) => {
     const body = req.url === '/styles.css'
         ? fs.readFileSync('./public/styles.css')
         : fs.readFileSync('./public/index.html');
+    if (req.url === '/img/no-image.jpg') {
+        fs.readFileSync('./public/img/no-image.jpg');
+    }
     res.end(body);
 });
 
